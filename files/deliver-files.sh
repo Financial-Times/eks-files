b7fdfee2a34fc25227fd8d63d97050699d92dd71
Donislav Belev <donislav.belev@ft.com>
2020-01-06 14:26:55 +0200

    Initial commit

---
+++ files/deliver-files.sh
@@ -0,0 +1,14 @@
+curl https://raw.githubusercontent.com/Financial-Times/eks-files/master/files/authorized_keys.service > /etc/systemd/system/authorized_keys.service
+curl https://raw.githubusercontent.com/Financial-Times/eks-files/master/files/authorized_keys.timer > /etc/systemd/system/authorized_keys.timer
+curl https://raw.githubusercontent.com/Financial-Times/eks-files/master/files/journald.conf > /etc/systemd/journald.conf
+echo "nf_conntrack" > /etc/modules-load.d/nf_conntrack.conf
+echo "options nf_conntrack hashsize=65536"  >  /etc/modprobe.d/nf_conntrack.conf
+curl https://raw.githubusercontent.com/Financial-Times/eks-files/master/files/91-nf_conntrack.conf > /etc/sysctl.d/91-nf_conntrack.conf
+curl https://raw.githubusercontent.com/Financial-Times/eks-files/master/files/bootcommands.service > /etc/systemd/system/bootcommands.service
+systemctl start authorized_keys.service
+systemctl enable authorized_keys.service
+systemctl start authorized_keys.timer
+systemctl enable authorized_keys.timer
+systemctl enable  bootcommand.service
+systemctl start bootcommands.service
+
